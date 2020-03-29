dda1b13cc46f367e43af5da1e2098dd07fa7a818
Piotr_W <46453412+kapitannemo12345@users.noreply.github.com>
2020-03-29 18:55:18 +0200

    dft fixed spectrum prime added

--- lab03/lab03.cpp
+++ lab03/lab03.cpp
@@ -11,8 +11,8 @@ using namespace std;
 const float  PI_F = 3.14159265358979f;
 
 float A = 1;
-float B = 9;
-float C = 4;
+float B = 1;
+float C = 5;
 
 float fs = 10;//czestotliwosc 
 
@@ -43,11 +43,11 @@ void DFT(vector<float> OY, DFT_Coeff &dft)
 	{
 		for (int n = 0; n < N; n++)
 		{
-			pr+= OY[n] * cos(-2*(PI_F* i * n));//k=i?
-			pi+= OY[n] * sin(-2*(PI_F* i * n));			
+			pr+= OY[n] * cos(-2*(PI_F* i * n)/N);//k=i?
+			pi+= OY[n] * sin(-2*(PI_F* i * n)/N);			
 		}
-		pr = pr / N;
-		pi = pi / N;
+		//pr = pr / N;
+		//pi = pi / N;
 
 		dft.real.push_back(pr);
 		dft.img.push_back(pi);
@@ -73,6 +73,51 @@ void amplitude_spectrum( DFT_Coeff dft, vector<float> &AS, vector<float> OY)//wi
 	}
 }
 
+void amplitude_spectrum_prime(vector<float> &AS, vector<float> &AS_P, vector<float> OY,float max)//widmo amplitudowe
+{
+	
+	for (int i=0;i<AS.size();i++)
+	{
+		if (abs(AS[i]) > max)
+		{
+			max = abs(AS[i]);
+		}
+
+	}
+
+	float threshold=max/10000;
+
+	for (int i = 0; i < AS.size(); i++)
+	{
+		if (AS[i]< threshold)
+		{
+			AS[i] = 0.0;
+		}
+
+	}
+
+	int N = OY.size();
+	float y;
+	int i = 0;
+	for (float x : AS)
+	{
+		
+		y = 10 * log10(AS[i]);
+		
+		if (AS[i] == 0)//omijanie widm dla których wartosc wynosi zero
+		{
+			AS_P.push_back(0);
+
+		}
+		else
+		{
+			AS_P.push_back(y);
+		}
+
+		i++;
+	}
+}
+
 void frequency_scale(DFT_Coeff dft, vector<float> &FS)//skala częstotliwości
 {
 	float y;
@@ -93,7 +138,7 @@ void signal_tone(vector<float> OX, vector<float> &OY)
 
 	for (float x : OX)
 	{
-		y = 1 * sin(2 * PI_F * B * OX[i] + C * PI_F);
+		y = A * sin(2 * PI_F * B * OX[i] + C * PI_F);
 		OY.push_back(y);
 		i++;
 	}
@@ -119,6 +164,24 @@ void data_file(vector<float> OX, vector<float> OY, string name)
 
 }
 
+void data_file2(vector<float> OX, string name)
+{
+	ofstream myfile;
+	myfile.open(name);
+	int i = 0;
+
+	//cout << "size ox:" << OX.size();
+
+	for (float x : OX)
+	{
+		myfile << OX[i] << ",";
+		i++;
+	}
+
+	myfile.close();
+
+}
+
 int main()
 {
 	//----zad2-----
@@ -127,24 +190,27 @@ int main()
 	vector<float> y1;
 	DFT_Coeff DFT1;
 	vector<float> AS1;
-	vector<float> FS1;
+	vector<float> AS2;
 
-	create_OX(0.0, 194, 0.1, x1);// nie za male fs? 0.1 0.001
+	vector<float> FS1;
+	vector<float> AS_P;
+
+	create_OX(0.0, 555, 0.1, x1);// nie za male fs? 0.1 0.001
 	signal_tone(x1, y1);
 	data_file(x1, y1, "function_s.txt");
+	//data_file2(x1, "x.txt");
+	//data_file2(y1, "y.txt");
 	DFT(y1,DFT1);
 	amplitude_spectrum(DFT1, AS1,y1);
+	amplitude_spectrum(DFT1, AS2, y1);
+	amplitude_spectrum_prime(AS2, AS_P,y1,0);
+	
 	frequency_scale(DFT1, FS1);
-
-
+	
 	data_file(FS1, AS1, "aspec1.txt");
+	data_file(FS1, AS_P, "aspec2.txt");
     
-
 	DFT_Coeff dft_value;
-
-
-
-
 
 }
 
