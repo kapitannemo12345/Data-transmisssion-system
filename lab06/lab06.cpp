478d38001877109da23136bd690a1823b8e461aa
Piotr_W <46453412+kapitannemo12345@users.noreply.github.com>
2020-04-18 19:22:27 +0200

    progress update

--- lab06/lab06.cpp
+++ lab06/lab06.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <vector>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -14,23 +15,104 @@ float A = 5;
 float B = 5;//4
 float C = 5;
 
-float fs = 1000;
+float Tb = 1;
+float N = 1;
+float fs = 1000;//TB*10 =fs !!!
+
+
 
 typedef unsigned char BYTE;
 
+string Binary_stream(int text)
+{
+	char buffer[33];
+	_itoa_s(text, buffer, 2);
+	string s(buffer);
+	return s;
+}
+
+void information_signal(vector<float> &OY,string s)
+{
+	float y;
+	int i = 0;
+	float czas_trwania = Tb * fs;
+	for (string::size_type i = 0; i < 10; i++) {//10 bo do 10 bitow ograniczyc
+		
+		if (s[i]=='1')
+		{
+			//cout << s[i] << ' ';
+			for (int j=0;j< czas_trwania;j++)
+			{
+				y = 1;
+				OY.push_back(y);
+				
+			}
+		}
+		else
+		{
+			//cout << ' ';
+			for (int j = 0; j < czas_trwania; j++)
+			{
+				y = 0;
+				OY.push_back(y);
+				
+			}
+		}
+
+		
+	}
+
+}
+
+void amplitude_keying(vector<float> OX, vector<float> IF, vector<float> &OY)
+{
+
+	
+	float y;
+	int i = 0;
+	float f = N / Tb;
+	float A1=1;
+	float A2=8;
+
+	for (float x : OX)
+	{
+
+		if (IF[i] == 1)
+		{
+			y = A1 * sin(2 * PI_F * f * OX[i] + 1 * PI_F);
+			OY.push_back(y);
+			i++;
+
+		}
+		else
+		{
+			y = A2 * sin(2 * PI_F * f * OX[i] + 1 * PI_F);
+			OY.push_back(y);
+			i++;
+
+		}
+		
+	}
 
 
-void signal_tone(vector<float> OX, vector<float> &OY)
+}
+
+
+void signal_tone(vector<float> OX, vector<float> &OY,float A,float f,float FI)
 {
 	//cout << "test";
 	float y;
 	int i = 0;
 
+
 	for (float x : OX)
 	{
-		y = 1 * sin(2 * PI_F * B * OX[i] + C * PI_F);
+		
+		y = A * sin(2 * PI_F * f * OX[i] + f * PI_F);
 		OY.push_back(y);
 		i++;
+
+		
 	}
 
 }
@@ -178,10 +260,47 @@ void data_file2(vector<float> OX, string name)
 
 int main()
 {
+	vector<float> x1;
+	vector<float> y1;
+	vector<float> IF1;
+
+	int i = 'ab';
+	string s;
+	s=Binary_stream(i);
+	cout << s << "\n";
 	
+	create_OX(0, 10000, 1 / fs, x1);
+	information_signal(IF1, s);
+
+	cout << "size OX:" << x1.size()<<"\n";
+	cout << "size IF1:" << IF1.size()<<"\n";
+
+	data_file2(x1, "x.txt");
+	data_file2(IF1, "IF1.txt");
+
+	amplitude_keying(x1, IF1, y1);
+	data_file2(y1, "y1.txt");
 
 
+	//int j = 'b';
+	//char buffer[33]; //the variable you will store i's new value (binary value) in
+	//char buffer2[33];
+	
+	//_itoa_s(i, buffer, 2);
+	//_itoa_s(j, buffer2, 2);
 
+	//printf("binary: %s\n", buffer);
 
+	//string s(buffer);
+	//string r(buffer2);
+
+	//cout << s<<"\n";
+	//cout << r << "\n";
+
+	//string t ;
+	//t += s;
+	//t += r;
+	//cout << t << "\n";
+	
 }
 
